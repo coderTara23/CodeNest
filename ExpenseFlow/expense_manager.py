29df274f1c7f5a290d8f6000519666e968b362ae
coderTara23 <tara239a@gmail.com>
2026-06-28 00:24:08 +0530

    Update expense_manager.py

--- ExpenseFlow/expense_manager.py
+++ ExpenseFlow/expense_manager.py
@@ -9,3 +9,12 @@ class ExpenseManager:
         }
         self.expenses.append(expense)
         print("Expense added successfully!")
+
+    def view_expenses(self):
+        if not self.expenses:
+            print("No expenses recorded.")
+            return
+
+        print("\nExpenses:")
+        for expense in self.expenses:
+            print(f"Amount: ₹{expense['amount']} | Category: {expense['category']}")
