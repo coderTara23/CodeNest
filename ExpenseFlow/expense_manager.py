a937468eae7bdc2d5b8bc6ed6ffb98b59ba244b8
coderTara23 <tara239a@gmail.com>
2026-06-28 00:31:53 +0530

    Added CSV feature

--- ExpenseFlow/expense_manager.py
+++ ExpenseFlow/expense_manager.py
@@ -1,14 +1,20 @@
+import csv
 class ExpenseManager:
     def __init__(self):
         self.expenses = []
 
     def add_expense(self, amount, category):
-        expense = {
-            "amount": amount,
-            "category": category
-        }
-        self.expenses.append(expense)
-        print("Expense added successfully!")
+    expense = {
+        "amount": amount,
+        "category": category
+    }
+    self.expenses.append(expense)
+
+    with open("expenses.csv", "a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([amount, category])
+
+    print("Expense added successfully and saved!")
 
     def view_expenses(self):
         if not self.expenses:
