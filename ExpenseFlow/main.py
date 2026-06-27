58448e5139c78453b1b79493903398ad47a27c54
coderTara23 <tara239a@gmail.com>
2026-06-28 00:22:24 +0530

    Updated  main.py with menu system

--- ExpenseFlow/main.py
+++ ExpenseFlow/main.py
@@ -1,7 +1,30 @@
-print("Welcome to ExpenseFlow 💸")
+from expense_manager import ExpenseManager
 
 def main():
-    print("Expense Tracker Started")
+    manager = ExpenseManager()
+
+    while True:
+        print("\n===== ExpenseFlow Menu =====")
+        print("1. Add Expense")
+        print("2. View Expenses")
+        print("3. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            amount = float(input("Enter amount: "))
+            category = input("Enter category: ")
+            manager.add_expense(amount, category)
+
+        elif choice == "2":
+            print(manager.expenses)
+
+        elif choice == "3":
+            print("Exiting ExpenseFlow...")
+            break
+
+        else:
+            print("Invalid choice. Try again.")
 
 if __name__ == "__main__":
     main()
